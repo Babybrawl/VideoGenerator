2177a55739762e0e4077af89665d65edc2c5d407
Distream <nicolas.babybrawl@gmail.com>
2024-09-26 16:05:24 +0200

    Retrait de fichiers volumineux et ajout avec Git LFS

--- check_and_run.py
+++ check_and_run.py
@@ -1,20 +1,3 @@
-import os
-import subprocess
-
-# Dossier à vérifier
-to_upload_folder = r"D:\test400\to_upload"
-
-def check_and_run():
-    # Vérifier si le dossier 'to_upload' contient des fichiers
-    if any(os.path.isfile(os.path.join(to_upload_folder, f)) for f in os.listdir(to_upload_folder)):
-        print("Des fichiers trouvés dans le dossier 'to_upload'. Lancement de run2.py...")
-        try:
-            subprocess.run(["python", "d:/test400/run2.py"], check=True)
-            print("run2.py a été exécuté avec succès.")
-        except subprocess.CalledProcessError as e:
-            print(f"Erreur lors de l'exécution de run2.py: {e}")
-    else:
-        print("Aucun fichier trouvé dans le dossier 'to_upload'.")
-
-# Exécution
-check_and_run()
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7ddcb7594a967ef48c839ff5994ac4527044f01aacb485757324a5012287105
+size 766
